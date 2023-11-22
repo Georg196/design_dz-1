4459e255db042a1d0f9b2d365d816f36f3ddc95a
George Kondrashevsky <146424140+Georg196@users.noreply.github.com>
2023-11-22 15:16:29 +0500

    Add files via upload

---
+++ add.js
@@ -0,0 +1,15 @@
+// const menuItems = document.querySelectorAll('.text-menu');
+// const line = document.querySelector('.line');
+
+// menuItems.forEach((item) => {
+//   item.addEventListener('click', () => {
+//     // Удаляем класс "active" у всех пунктов меню
+//     menuItems.forEach((menuItem) => menuItem.classList.remove('active'));
+
+//     // Добавляем класс "active" только к выбранному пункту меню
+//     item.classList.add('active');
+
+//     // Перемещаем линию к активному пункту меню
+//     line.style.transform = `translateY(${item.offsetTop}px)`;
+//   });
+// });
